44eda843567e349e8217ce0a30448425efd86031
KireinaHoro <i@jsteward.moe>
2017-11-28 00:00:54 +0800

    update submitting scripts for botzone

--- generate_submission.sh
+++ generate_submission.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-( cat gameprocess.h gameai.h botzonerunner.h gameprocess.cpp gameai.cpp botzonerunner.cpp | sed -e '/#include "/d' && cat fake_main )> a.cpp
-g++ a.cpp -o a -Wall -Werror -Wextra -Wconversion
+
+( cat gameprocess.h gameai.h botzonerunner.h gameprocess.cpp gameai.cpp botzonerunner.cpp | sed -e '/#include "/d' && cat fake_main )> submission.cpp
+g++ submission.cpp -o submission -Wall -Werror -Wextra -Wconversion
 
