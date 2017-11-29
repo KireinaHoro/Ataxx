8af6f1074733aafa30e343af584666f670402a22
KireinaHoro <i@jsteward.moe>
2017-11-29 09:25:44 +0800

    add -O2 to compile flags

--- generate_submission.sh
+++ generate_submission.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ( cat gameprocess.h gameai.h botzonerunner.h gameprocess.cpp gameai.cpp botzonerunner.cpp | sed -e '/#include "/d' && cat fake_main )> submission.cpp
-g++ submission.cpp -o submission -Wall -Werror -Wextra -Wconversion -pthread
+g++ submission.cpp -o submission -Wall -Werror -Wextra -Wconversion -pthread -O2
 
