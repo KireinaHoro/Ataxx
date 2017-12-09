de1422b6623a1496f4cfccce352cdbb410eb9b4f
KireinaHoro <i@jsteward.moe>
2017-12-10 00:39:06 +0800

    finished japanese translation (credit goes to farseerfc)

--- languages/Translation_ja_JP.ts
+++ languages/Translation_ja_JP.ts
@@ -6,32 +6,32 @@
     <message>
         <location filename="../highscoredialog.cpp" line="9"/>
         <source>Start date</source>
-        <translation type="unfinished"></translation>
+        <translation>開始時間</translation>
     </message>
     <message>
         <location filename="../highscoredialog.cpp" line="10"/>
         <source>End date</source>
-        <translation type="unfinished"></translation>
+        <translation>終了時間</translation>
     </message>
     <message>
         <location filename="../highscoredialog.cpp" line="11"/>
         <source>Starting Player</source>
-        <translation type="unfinished"></translation>
+        <translation>先手プレイヤー</translation>
     </message>
     <message>
         <location filename="../highscoredialog.cpp" line="12"/>
         <source>Player won</source>
-        <translation type="unfinished"></translation>
+        <translation>勝利プレイヤー</translation>
     </message>
     <message>
         <location filename="../highscoredialog.cpp" line="13"/>
         <source>Score</source>
-        <translation type="unfinished"></translation>
+        <translation>スコア</translation>
     </message>
     <message>
         <location filename="../highscoredialog.cpp" line="36"/>
         <source>High Score Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>ハイスコアランキング</translation>
     </message>
 </context>
 <context>
@@ -39,249 +39,249 @@
     <message>
         <location filename="../mainwindow.cpp" line="48"/>
         <source>Ready</source>
-        <translation type="unfinished"></translation>
+        <translation>待機</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="80"/>
         <source>Ataxx Main Window</source>
-        <translation type="unfinished"></translation>
+        <translation>Ataxx主画面</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="574"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>ファイル(&amp;F)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="582"/>
         <source>&amp;Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>設定(&amp;S)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="584"/>
         <source>&amp;White player</source>
-        <translation type="unfinished"></translation>
+        <translation>白プレイヤー(&amp;W)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="593"/>
         <source>&amp;High score</source>
-        <translation type="unfinished"></translation>
+        <translation>ハイスコア(&amp;H)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="483"/>
         <location filename="../mainwindow.cpp" line="597"/>
         <source>&amp;About</source>
-        <translation type="unfinished"></translation>
+        <translation>Ataxxについて(&amp;A)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="402"/>
         <source>&amp;New game</source>
-        <translation type="unfinished"></translation>
+        <translation>新規対局(&amp;N)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="413"/>
         <source>E&amp;xit</source>
-        <translation type="unfinished"></translation>
+        <translation>終了(&amp;X)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="417"/>
         <source>&amp;Load game</source>
-        <translation type="unfinished"></translation>
+        <translation>対局を開く(&amp;L)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="422"/>
         <source>&amp;Save game</source>
-        <translation type="unfinished"></translation>
+        <translation>対局を保存(&amp;S)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="194"/>
         <location filename="../mainwindow.cpp" line="377"/>
         <source>Restart</source>
-        <translation type="unfinished"></translation>
+        <translation>再開</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="209"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>終了</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="362"/>
         <location filename="../mainwindow.cpp" line="368"/>
         <source>The game is over!</source>
-        <translation type="unfinished"></translation>
+        <translation>ゲーム終了！</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="371"/>
         <source>Black won this round!</source>
-        <translation type="unfinished"></translation>
+        <translation>黒が本局を勝利した！</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="373"/>
         <source>White won this round!</source>
-        <translation type="unfinished"></translation>
+        <translation>白が本局を勝利した！</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="375"/>
         <source>Score is %1. Restart or close?</source>
-        <translation type="unfinished"></translation>
+        <translation>スコアは%1。もう一局始まるか、或いは終了するか？</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="397"/>
         <location filename="../mainwindow.cpp" line="737"/>
         <location filename="../mainwindow.cpp" line="740"/>
         <source>Black</source>
-        <translation type="unfinished"></translation>
+        <translation>黒</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="397"/>
         <location filename="../mainwindow.cpp" line="737"/>
         <location filename="../mainwindow.cpp" line="740"/>
         <source>White</source>
-        <translation type="unfinished"></translation>
+        <translation>白</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="397"/>
         <source>&apos;s turn.</source>
-        <translation type="unfinished"></translation>
+        <translation>の番。</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="419"/>
         <source>Load the saved game</source>
-        <translation type="unfinished"></translation>
+        <translation>保存した対局を読み取る</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="424"/>
         <source>Save the current game</source>
-        <translation type="unfinished"></translation>
+        <translation>今の対局を書き込む</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="427"/>
         <source>&amp;Clear saved game</source>
-        <translation type="unfinished"></translation>
+        <translation>保存した対局をクリアする(&amp;C)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="428"/>
         <source>Clear the saved game</source>
-        <translation type="unfinished"></translation>
+        <translation>保存した対局をクリアする</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="431"/>
         <source>&amp;Show high score</source>
-        <translation type="unfinished"></translation>
+        <translation>ハイスコア一覧(&amp;S)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="432"/>
         <source>Show saved high scores</source>
-        <translation type="unfinished"></translation>
+        <translation>保存したハイスコアを一覧</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="439"/>
         <source>&amp;Clear high score</source>
-        <translation type="unfinished"></translation>
+        <translation>保存したハイスコアをクリアする(&amp;C)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="440"/>
         <source>Clear saved high scores</source>
-        <translation type="unfinished"></translation>
+        <translation>保存したハイスコアをクリアする</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="448"/>
         <source>Human</source>
-        <translation type="unfinished"></translation>
+        <translation>人間プレイヤー</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="450"/>
         <source>Human as white player</source>
-        <translation type="unfinished"></translation>
+        <translation>人間プレイヤーは白を取る</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="456"/>
         <source>AI</source>
-        <translation type="unfinished"></translation>
+        <translation>人工知能</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="458"/>
         <source>AI as white player</source>
-        <translation type="unfinished"></translation>
+        <translation>人工知能は白を取る</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="471"/>
         <source>Black starts</source>
-        <translation type="unfinished"></translation>
+        <translation>黒は先手</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="484"/>
         <source>About this application</source>
-        <translation type="unfinished"></translation>
+        <translation>このアプリについて</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="568"/>
         <source>Current language changed to %1</source>
-        <translation type="unfinished"></translation>
+        <translation>言語は%1に切り替えた</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="588"/>
         <source>&amp;Language</source>
-        <translation type="unfinished"></translation>
+        <translation>言語(&amp;L)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="604"/>
         <source>About Ataxx</source>
-        <translation type="unfinished"></translation>
+        <translation>Ataxxについて</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="605"/>
         <source>&lt;b&gt;Ataxx&lt;/b&gt; with C++14 and Qt 5. Explore to find out more!</source>
-        <translation type="unfinished"></translation>
+        <translation>C++14とQt5で開発した &lt;b&gt;Ataxx&lt;/b&gt;。もっと体験しよう！</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="630"/>
         <source>Reset running game</source>
-        <translation type="unfinished"></translation>
+        <translation>今の対局を再開する</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="631"/>
         <source>Do you want to reset the current game to apply changes?</source>
-        <translation type="unfinished"></translation>
+        <translation>変更を適用するため、今の対局を再開しますか？</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="667"/>
         <source>Exiting Ataxx</source>
-        <translation type="unfinished"></translation>
+        <translation>終了する</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="668"/>
         <source>Save running game</source>
-        <translation type="unfinished"></translation>
+        <translation>今の対局を書き込む</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="669"/>
         <source>Do you want to save your running game?</source>
-        <translation type="unfinished"></translation>
+        <translation>今の対局を書き込みますか？</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="711"/>
         <source>Game loading failed</source>
-        <translation type="unfinished"></translation>
+        <translation>対局を読み取れなかった</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="711"/>
         <source>The save data seems to be corrupted. A new game will be created.</source>
-        <translation type="unfinished"></translation>
+        <translation>保存した対局データは破損したようです。新しい対局が作成されます。</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="713"/>
         <source>Game loading finished</source>
-        <translation type="unfinished"></translation>
+        <translation>対局を読み取りました</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="713"/>
         <source>The saved game has been successfully loaded.</source>
-        <translation type="unfinished"></translation>
+        <translation>保存した対局は成功に読み取りました。</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="740"/>
         <source>White (AI)</source>
-        <translation type="unfinished"></translation>
+        <translation>白（人工知能）</translation>
     </message>
 </context>
 </TS>
